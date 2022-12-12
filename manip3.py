dee4a3bde8322fc0e22f059bc85953831fbe49dc
Aude510 <dunae97300@outlook.fr>
2022-12-12 14:19:07 +0100

    reception fm

--- manip3.py
+++ manip3.py
@@ -95,7 +95,7 @@ class manip3(gr.top_block, Qt.QWidget):
         )
         self.uhd_usrp_source_0.set_center_freq(center_freq_mod, 0)
         self.uhd_usrp_source_0.set_gain(80, 0)
-        self.uhd_usrp_source_0.set_antenna('RX2', 0)
+        self.uhd_usrp_source_0.set_antenna('TX/RX', 0)
         self.uhd_usrp_source_0.set_samp_rate(samp_rate)
         self.uhd_usrp_source_0.set_time_unknown_pps(uhd.time_spec())
         self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
