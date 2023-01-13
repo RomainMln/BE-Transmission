a927655795f637ac600e96f47d10e5fe664d36f5
RomainMln <moulin.romain@hotmail.com>
2023-01-13 10:23:01 +0100

    Rick Roll

--- script.sh
+++ script.sh
@@ -1,8 +1,10 @@
+
 #!/bin/bash
 
 
-tail -c +7 fichier-recu.txt  > shift.txt
-f=`cat shift.txt`
+
 echo $f
-#file=($file>>2)
-echo ($f>>2)
+f=$(($f>>2))
+echo $f
+tail -c +7 fichier-recu.txt  > shift.txt
+
